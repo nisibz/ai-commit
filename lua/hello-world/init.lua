d8989dbed660512b0ced764f8088be71b7638f6e
Nantachai <nantachai.s@swiftlet.co.th>
2024-12-05 16:38:53 +0700

    Update hello function to append "Hello World Updated!" at cursor position in the current line when called, instead of printing it.

--- lua/hello-world/init.lua
+++ lua/hello-world/init.lua
@@ -1,7 +1,10 @@
 local M = {}
 
 function M.hello()
-	print("Hello World Updated!")
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local line = vim.api.nvim_get_current_line()
+	local new_line = line:sub(1, col) .. "Hello World Updated!" .. line:sub(col + 1)
+	vim.api.nvim_set_current_line(new_line)
 end
 
 function M.setup(opts)
